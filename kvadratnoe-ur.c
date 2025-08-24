a9b5fdfcdeb73c7cda1a606d4eb889faabeceb27
kisich5812 <iaroslavvlasovvlasov@ya.ru>
2025-08-24 13:45:42 +0300

    Для констант используется enum, улучшены структуры

--- kvadratnoe-ur.c
+++ kvadratnoe-ur.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
-#include <math.h>
 #include <float.h>
+#include <math.h>
 #include <assert.h>
+
+struct coefficients {
+        double a;
+        double b;
+        double c;
+};
+
+struct answers {
+	double x1;
+        double x2;
+        int n_roots;
+};
 
 int enter_coef(struct coefficients* enter_values);
 int check_enter(double* double_number);
@@ -10,38 +22,45 @@ int solve(struct coefficients coef, struct answers* roots);
 int linear_case(struct coefficients coef, struct answers* roots);
 int square_case(struct coefficients coef, struct answers* roots);
 int compare_doubles(double ch1, double ch2);
-int print_answer(struct answers roots, int nRoots);
+int print_answer(struct answers roots);
 int continue_solve();
-
-struct coefficients {
-	double a;
-	double b;
-	double c;
-};
-
-struct answers {
-	double x1;
-	double x2;
-};
 
 const double PREDEL = pow(10, -DBL_DIG);
 
+const int standart_return = 1;
+enum number_of_roots {
+        inf_roots = -1,
+        no_roots,
+        one_root,
+        two_roots,
+        no_calculate_err
+};
+
+enum compare {
+        less = -1,
+        equal,
+        more,
+        no_compare_err = 3
+};
+enum continue_sol {
+        no=0,
+        yes=1
+};
+
 int main(void)
 {
-	struct coefficients coef = {NAN, NAN, NAN};
-	struct answers roots = {NAN, NAN};
-	
+	struct coefficients coef = {.a = NAN, .b = NAN, .c = NAN};
+        struct answers roots = {.x1 = NAN, .x2 = NAN, .n_roots = no_calculate_err};
 	printf("Эта программа предназначена для решения квадратных уравнений\n");
         printf("представленных в виде a*x^2+b*x+c=0.\n");
-	do
+        do
         {
                 enter_coef(&coef);
-                int nRoots = solve(coef, &roots);
-                print_answer(roots, nRoots);
+                assert(solve(coef, &roots) != 3);
+                print_answer(roots);
                 printf("Хотите ли вы решаить ещё одно уравнение? [y/n]: ");
         } while (continue_solve());
-	//printf("a = %lg, b = %lg, c = %lg\n", coef.a, coef.b, coef.c);
-	return 0;
+        return 0;
 }
 
 int solve(struct coefficients coef, struct answers* roots)
@@ -50,60 +69,94 @@ int solve(struct coefficients coef, struct answers* roots)
         assert(!isnan(coef.a));
         assert(!isnan(coef.b));
         assert(!isnan(coef.c));
-        assert(&(roots -> x1));
-        assert(&(roots -> x2));
+        assert(roots);
 
-        if (compare_doubles(coef.a, 0) == 0)                 // разбор случаев
+        if (compare_doubles(coef.a, 0) == equal)                 // разбор случаев
                 return linear_case(coef, roots);
+        else if (compare_doubles(coef.b, 0) == equal && compare_doubles(coef.c, 0) == equal)
+        {
+                roots->x1 = 0;
+                roots->n_roots = one_root;
+		return one_root;
+        }
+        else if (compare_doubles(coef.c, 0) == equal)
+        {
+                roots->x2 = -coef.b/coef.a;
+                roots->x1 = 0;
+                roots->n_roots = two_roots;
+		return two_roots;
+        }
         else
-                return square_case(coef, roots);    // TODO: загнать коды возврата в enum
-        return 3;       // возвращаем тройку как сигнал об ошибке
+                return square_case(coef, roots);
+        return no_calculate_err;       // возвращаем ошибку(нет расчётов)
 }
 
 int linear_case(struct coefficients coef, struct answers* roots)
 {
-        if (compare_doubles(coef.b, 0) == 0)
+        if (compare_doubles(coef.b, 0) == equal)
         {
-                if (compare_doubles(coef.c, 0) == 0)
-                        return -1;              // 0*x^2 + 0*x + 0 = 0 бесконечное множество решений, возвращаем -1
-                else
-                        return 0;               // 0*x^2 + 0*x + c = 0 нет корней, возвращаем 0
-        }
+                if (compare_doubles(coef.c, 0) == equal)
+		{
+                        roots->n_roots = inf_roots;
+			return inf_roots;               // 0*x^2 + 0*x + 0 = 0 бесконечное множество решений, возвращаем -1
+		}
+		else
+		{
+                        roots->n_roots = no_roots;
+			return no_roots;                // 0*x^2 + 0*x + c = 0 нет корней, возвращаем 0
+		}
+	}
         else
         {
-                if (compare_doubles(coef.c, 0) == 0) // 0*x^2 + b*x + 0 = 0 единственный корень, возвращаем 1
-                        roots -> x1 = 0;
+                if (compare_doubles(coef.c, 0) == equal)// 0*x^2 + b*x + 0 = 0 единственный корень, возвращаем 1
+                        roots->x1 = 0;
                 else
-                        roots -> x1 = -coef.c/coef.b;             // 0*x^2 + b*x + с = 0 единственный корень, возвращаем 1
-                return 1;
+                        roots->x1 = -coef.c/coef.b;   // 0*x^2 + b*x + с = 0 единственный корень, возвращаем 1
+                roots->n_roots = one_root;
+		return one_root;
         }
-        return 3; // код ошибки
+        
+	return no_calculate_err; // код ошибки
 }
 
 int square_case(struct coefficients coef, struct answers* roots)
 {
-        double d = coef.b * coef.b - 4 * coef.a * coef.c;         // считаем дискриминант
-        double sqrt_d = sqrt(d);        // и корень из него
-        if (compare_doubles(d, 0) == 1) // a*x^2 + b*x + c = 0
+        double d = coef.b * coef.b - 4 * coef.a * coef.c;       // считаем дискриминант
+        double sqrt_d = sqrt(d);                                // и корень из него
+        
+	if (compare_doubles(d, 0) == more)                              // a*x^2 + b*x + c = 0
         {
-                roots -> x1 = (-coef.b + sqrt_d) / (2*coef.a); // TODO: добавить случай c = 0
-                roots -> x2 = (-coef.b - sqrt_d) / (2*coef.a);
-                if (compare_doubles(roots -> x1, roots -> x2) == 0)
-                        return 1;
-                else
-                        return 2;
+                roots->x1 = (-coef.b + sqrt_d) / (2*coef.a);
+                roots->x2 = (-coef.b - sqrt_d) / (2*coef.a);
+                if (compare_doubles(roots->x1, roots->x2) == equal)
+		{
+                        roots->n_roots = one_root;
+			return one_root;
+		}
+		else
+		{
+                        roots->n_roots = two_roots;
+			return two_roots;
+		}
         }
-        if (compare_doubles(d, 0) == 0)
+        
+	if (compare_doubles(d, 0) == equal)
         {
-                if(compare_doubles(coef.b, 0) == 0)
-                        roots -> x1 = 0;
+                if(compare_doubles(coef.b, 0) == equal)
+                        roots->x1 = 0;
                 else
-                        roots -> x1 = -coef.b/(2 * coef.a);
-                return 1;
+                        roots->x1 = -coef.b/(2 * coef.a);
+                roots->n_roots = one_root;
+		return one_root;
         }
-        if(compare_doubles(d, 0) == -1)
-                return 0;
-        return 3; // код ошибки
+        
+	if(compare_doubles(d, 0) == less)
+	{
+		roots->n_roots = no_roots;
+                return no_roots;
+	}
+        
+	return no_calculate_err; // код ошибки
 }
 
 int compare_doubles(double ch1, double ch2)             // 0 - ch1 == ch2 1 - ch1 > ch2 -1 - ch1 < ch2
@@ -112,26 +165,24 @@ int compare_doubles(double ch1, double ch2)             // 0 - ch1 == ch2 1 - ch
         assert(!isnan(ch2));
 
         if (fabs(ch1 - ch2) < PREDEL)   // ==
-                return 0;
+                return equal;
         else if (ch1 - ch2 > 0)         // ch1 > ch2
-                return 1;
+                return more;
         else if (ch1 - ch2 < 0)         // ch1 < ch2
-                return -1;
-        return 3;                       // код ошибки
+                return less;
+        return no_compare_err;          // код ошибки
 }
 
 int enter_coef(struct coefficients* enter_values)
 {
-        assert(&(enter_values -> a));
-        assert(&(enter_values -> b));
-        assert(&(enter_values -> c));
+        assert(enter_values);
         printf("Введите коэффициент a:\n");
-        check_enter(&(enter_values -> a));
+        check_enter(&(enter_values->a));
         printf("Введите коэффициент b:\n");
-        check_enter(&(enter_values -> b));
+        check_enter(&(enter_values->b));
         printf("Введите коэффициент c:\n");
-        check_enter(&(enter_values -> c));
-        return 1;
+        check_enter(&(enter_values->c));
+        return standart_return;
 }
 
 int check_enter(double* double_number)
@@ -146,7 +197,7 @@ int check_enter(double* double_number)
                 if (no_input)
                         printf("Ошибка ввода. Попробуйте ещё раз:\n");
         }
-        return 1;
+        return standart_return;
 }
 
 int clean_buffer()
@@ -157,33 +208,33 @@ int clean_buffer()
         return quantity_sim;
 }
 
-int print_answer(struct answers roots, int nRoots)
+int print_answer(struct answers roots)
 {
-        if (nRoots == -1)
+        if (roots.n_roots == inf_roots)
                 printf("х - любое число\n");
-        else if (nRoots == 0)
+        else if (roots.n_roots == no_roots)
                 printf("Уравнение не имеет корней\n");
-        else if (nRoots == 1)
+        else if (roots.n_roots == one_root)
         {
                 printf("Уравнение имеет один корень:\n");
                 printf("x = %g\n", roots.x1);
         }
-        else if (nRoots == 2)
+        else if (roots.n_roots == two_roots)
         {
                 printf("Уравнение имеет два корня:\n");
                 printf("x1 = %g\nx2 = %g\n", roots.x1, roots.x2);
         }
         else
                 printf("При выполнении произошла ошибка!\n");
-        return 1;
+        return standart_return;
 }
 
 int continue_solve()
 {
         int ch = getchar();
         if (ch == 'y')
-                return 1;
+                return yes;
         if (ch == 'n')
-                return 0;
-        return 0;
+                return no;
+        return no;
 }
