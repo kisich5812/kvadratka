67642779fa726b59578cc3985ee62bcd16232ef1
kisich5812 <iaroslavvlasovvlasov@ya.ru>
2025-08-28 13:17:30 +0300

    Добавлены флаги -h --help

--- flags.c
+++ flags.c
@@ -4,7 +4,7 @@
 
 int processing_flags(int val_args, char* args[])
 {
-	if (val_args > 1)
+	/*if (val_args > 1)
         {
                 if (val_args == 2 && strcmp(args[1], "--help") == 0)
                 {
@@ -16,8 +16,23 @@ int processing_flags(int val_args, char* args[])
 			printf("Неизвестный аргумент %s\n", args[1]);
 			return 0;
 		}
-        }
-	return 1;
+        
+	}*/
+	bool do_prog = 1;
+	for(int i = 1; i<val_args; i++)
+	{
+		if (strcmp(args[i], "-h") == 0 || strcmp(args[i], "--help") == 0)
+		{
+			print_help(args[0]);
+			do_prog = 0;
+		}
+		else
+		{
+			printf("Неизвестный аргумент %s\n", args[i]);
+			do_prog = 0;
+		}
+	}
+	return do_prog;
 }
 
 int print_help(char* prog_name)
@@ -26,6 +41,6 @@ int print_help(char* prog_name)
 	printf("%s [опции]\n", prog_name);
 	printf("Опции:\n");
 	printf("\t[без опций] решение квадратных уравнений\n");
-	printf("\t--help вывести данную справку\n");
+	printf("\t-h, --help вывести данную справку\n");
 	return 1;
 }
